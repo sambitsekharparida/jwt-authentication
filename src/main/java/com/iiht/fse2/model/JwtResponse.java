9e47d1fc97444f50e464ceada60ad016ea4dab58
sambitsekharparida <sambitsekharparida@outlook.com>
2022-09-11 13:01:46 +0530

    first commit

---
+++ src/main/java/com/iiht/fse2/model/JwtResponse.java
@@ -0,0 +1,15 @@
+package com.iiht.fse2.model;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse implements Serializable {
+
+	private static final long serialVersionUID = -8091879091924046844L;
+	private final String jwttoken;
+
+}
